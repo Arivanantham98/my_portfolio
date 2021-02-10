5476d6a7e0f140d05e82d331dacca58523cd8319
Arivanantham98 <ari.sparke@gmail.com>
2021-02-11 00:08:28 +0530

    new alter

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import Headroom from 'react-headroom';
 import './Header.css';
-import StyleContext from '../../contexts/StyleContext';
+
 
 import {
   greeting,
